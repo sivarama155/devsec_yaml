b346987409fa2e9140089d8cb543037623a00455
sivaramprasad <sivaramprasaditrajula@gmail.com>
2024-04-01 13:25:54 +0530

    added data

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,9 @@
 /* groovylint-disable-next-line EmptyMethod */
 /* groovylint-disable-next-line MethodReturnTypeRequired */
 def call() {
+    environment {
+        SCANNER_HOME = tool 'sonar-scanner'
+    }
     withSonarQubeEnv('sonar') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
                     -Dsonar.projectKey=Netflix '''
